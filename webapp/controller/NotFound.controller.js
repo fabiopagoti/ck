d26d58c8f77a3582c867db61926ea63ff21fd68b
Fábio Luiz Esperati Pagoti <fabiopagoti@gmail.com>
2017-10-03 01:18:07 +0000

    template correto

---
+++ webapp/controller/NotFound.controller.js
@@ -0,0 +1,19 @@
+sap.ui.define([
+		"pm/tlsup/controller/BaseController"
+	], function (BaseController) {
+		"use strict";
+
+		return BaseController.extend("pm.tlsup.controller.NotFound", {
+
+			/**
+			 * Navigates to the worklist when the link is pressed
+			 * @public
+			 */
+			onLinkPressed : function () {
+				this.getRouter().navTo("worklist");
+			}
+
+		});
+
+	}
+);
